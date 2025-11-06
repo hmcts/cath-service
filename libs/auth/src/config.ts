d2b45f052e42df5d107e660e164a6ade629d7473
junaidiqbalmoj <84805836+junaidiqbalmoj@users.noreply.github.com>
2025-11-06 14:40:00 +0000

    move code to another file

--- libs/auth/src/config.ts
+++ libs/auth/src/config.ts
@@ -1,1 +1,8 @@
-export * from "./config/config.js";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export const pageRoutes = { path: path.join(__dirname, "pages") };
+export const moduleRoot = __dirname;
