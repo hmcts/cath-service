e868a406369cd407afbe0f3cd0210df2173dd235
junaidiqbalmoj <84805836+junaidiqbalmoj@users.noreply.github.com>
2025-11-11 14:54:06 +0000

    refactoring

--- libs/auth/src/index.ts
+++ libs/auth/src/index.ts
@@ -3,7 +3,7 @@ export { getCftIdamConfig, isCftIdamConfigured } from "./config/cft-idam-config.
 export { configurePassport } from "./config/passport-config.js";
 export { isSsoConfigured } from "./config/sso-config.js";
 export { requireAuth } from "./middleware/authenticate.js";
-export { requireRole } from "./middleware/authorise.js";
+export { blockUserAccess, requireRole } from "./middleware/authorise.js";
 export { authNavigationMiddleware } from "./middleware/navigation.js";
 export { buildNavigationItems, buildVerifiedUserNavigation } from "./middleware/navigation-helper.js";
 export { GET as ssoCallbackHandler } from "./pages/callback/sso.js";
