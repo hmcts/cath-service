29de3801f84f128e3acbea9f79a7af7f9730f9f7
junaidiqbalmoj <84805836+junaidiqbalmoj@users.noreply.github.com>
2025-11-28 09:24:09 +0000

    fix code review comments

--- apps/postgres/prisma/migrations/20251126100649_blob_ingestion/migration.sql
+++ apps/postgres/prisma/migrations/20251126100649_blob_ingestion/migration.sql
@@ -1,9 +1,12 @@
+-- CreateExtension
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
 -- AlterTable
 ALTER TABLE "artefact" ADD COLUMN     "no_match" BOOLEAN NOT NULL DEFAULT false;
 
 -- CreateTable
 CREATE TABLE "ingestion_log" (
-    "id" UUID NOT NULL,
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
     "timestamp" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "source_system" TEXT NOT NULL,
     "court_id" TEXT NOT NULL,
@@ -11,7 +14,8 @@ CREATE TABLE "ingestion_log" (
     "error_message" TEXT,
     "artefact_id" UUID,
 
-    CONSTRAINT "ingestion_log_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "ingestion_log_pkey" PRIMARY KEY ("id"),
+    CONSTRAINT "fk_blob_artefact" FOREIGN KEY ("artefact_id") REFERENCES "artefact"("artefact_id") ON DELETE SET NULL
 );
 
 -- CreateIndex
