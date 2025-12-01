203d22c25ef8fd764e0274618182bc908253b9cc
junaidiqbalmoj <84805836+junaidiqbalmoj@users.noreply.github.com>
2025-12-01 14:33:29 +0000

    chore: remove old notification library (singular)
    The remote branch uses 'notifications' (plural) with better structure.
    Removed the local 'notification' directory to avoid confusion.

--- libs/notification/src/index.ts
+++
@@ -1,2 +0,0 @@
-export { sendPublicationNotifications } from "./notification-service.js";
-export { createNotificationLog, findNotificationLogsByPublicationId, findNotificationLogsByUserId } from "./repository/queries.js";
