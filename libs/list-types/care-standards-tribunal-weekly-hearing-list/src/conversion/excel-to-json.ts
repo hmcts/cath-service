f5aedc3bf1aca07c012fda79e0e370a3954faaac
junaidiqbalmoj <84805836+junaidiqbalmoj@users.noreply.github.com>
2025-12-05 09:48:09 +0000

    fix code review comment

--- libs/list-types/care-standards-tribunal-weekly-hearing-list/src/conversion/excel-to-json.ts
+++
@@ -1,6 +0,0 @@
-import { convertExcelForListType } from "@hmcts/list-types-common";
-import type { CareStandardsTribunalHearing } from "../models/types.js";
-
-export async function convertExcelToJson(buffer: Buffer): Promise<CareStandardsTribunalHearing[]> {
-  return convertExcelForListType(9, buffer) as Promise<CareStandardsTribunalHearing[]>;
-}
--- libs/list-types/care-standards-tribunal-weekly-hearing-list/src/index.ts
+++ libs/list-types/care-standards-tribunal-weekly-hearing-list/src/index.ts
@@ -1,4 +1,3 @@
 import "./conversion/cst-config.js"; // Register converter on module load
 
-export * from "./conversion/excel-to-json.js";
 export * from "./validation/json-validator.js";
