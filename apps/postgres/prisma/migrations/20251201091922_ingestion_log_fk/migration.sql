fefea2fac00ae9605aaf3d827a0137165c6554a2
junaidiqbalmoj <84805836+junaidiqbalmoj@users.noreply.github.com>
2025-12-15 09:16:15 +0000

    fix end to end test build issue

--- apps/postgres/prisma/migrations/20251201091922_ingestion_log_fk/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE "ingestion_log" DROP CONSTRAINT "fk_blob_artefact";
-
--- AlterTable
-ALTER TABLE "ingestion_log" ALTER COLUMN "id" DROP DEFAULT;
-
--- AddForeignKey
-ALTER TABLE "ingestion_log" ADD CONSTRAINT "ingestion_log_artefact_id_fkey" FOREIGN KEY ("artefact_id") REFERENCES "artefact"("artefact_id") ON DELETE SET NULL ON UPDATE CASCADE;
