4db77e20d17a861c9bb9208899dcb8add44f67bf
junaidiqbalmoj <84805836+junaidiqbalmoj@users.noreply.github.com>
2025-12-18 13:44:41 +0000

    fix sjp press list bugs

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -29,8 +29,8 @@ export async function createApp(): Promise<Express> {
     })
   );
 
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json({ limit: "10mb" }));
+  app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
   const routeMounts = [{ path: `${__dirname}/routes` }, blobIngestionRoutes, locationRoutes];
 
