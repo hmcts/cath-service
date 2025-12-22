c5f151c4c3e5143ca28f3a91a9b09963d167cb07
junaidiqbalmoj <84805836+junaidiqbalmoj@users.noreply.github.com>
2025-12-22 09:38:46 +0000

    remove postcode validation.
    It was done by source system

--- libs/list-types/common/src/index.ts
+++ libs/list-types/common/src/index.ts
@@ -17,6 +17,5 @@ export {
 export { convertListTypeNameToKebabCase, validateListTypeJson } from "./list-type-validator.js";
 export { type ListType, mockListTypes, SJP_PRESS_LIST_ID, SJP_PUBLIC_LIST_ID } from "./mock-list-types.js";
 export * from "./sjp/json-parser.js";
-export * from "./sjp/postcode-validator.js";
 export * from "./sjp/sjp-paginator.js";
 export * from "./sjp/sjp-service.js";
