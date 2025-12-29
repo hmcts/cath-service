f31afbca96ae7009df9364ad70afeeb8008cd16c
junaidiqbalmoj <84805836+junaidiqbalmoj@users.noreply.github.com>
2025-12-29 11:36:24 +0000

    remove unnecessary code

--- libs/list-types/sjp-public-list/src/pages/sjp-selection/cy.ts
+++
@@ -1,6 +0,0 @@
-export const cy = {
-  title: "Beth ydych chi eisiau ei weld o'r Weithdrefn Cyfiawnder Sengl?",
-  back: "Yn ôl",
-  publicListPrefix: "Rhestr Gyhoeddus SJP – ",
-  pressListPrefix: "Rhestr y Wasg SJP – "
-};
--- libs/list-types/sjp-public-list/src/pages/sjp-selection/en.ts
+++
@@ -1,6 +0,0 @@
-export const en = {
-  title: "What do you want to view from Single Justice Procedure?",
-  back: "Back",
-  publicListPrefix: "SJP Public List – ",
-  pressListPrefix: "SJP Press List – "
-};
