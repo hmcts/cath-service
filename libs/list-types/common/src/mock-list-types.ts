caa5d482f819d2573f43babb37614c8f4bc06a23
kian.kwa <kian.kwa@justice.gov.uk>
2026-01-15 16:29:02 +0000

    Update list type Welsh friendly names

--- libs/list-types/common/src/mock-list-types.ts
+++ libs/list-types/common/src/mock-list-types.ts
@@ -95,7 +95,7 @@ export const mockListTypes: ListType[] = [
     id: 10,
     name: "CIVIL_COURTS_RCJ_DAILY_CAUSE_LIST",
     englishFriendlyName: "Civil Courts at the RCJ Daily Cause List",
-    welshFriendlyName: "Rhestr Achosion Dyddiol y Llysoedd Sifil yn y Llys Barn Brenhinol",
+    welshFriendlyName: "Rhestr Achosion Dyddiol Llys Sifil yn y Llysoedd Barn Brenhinol",
     provenance: "MANUAL_UPLOAD",
     urlPath: "civil-courts-rcj-daily-cause-list",
     isNonStrategic: true
@@ -104,7 +104,7 @@ export const mockListTypes: ListType[] = [
     id: 11,
     name: "COUNTY_COURT_LONDON_CIVIL_DAILY_CAUSE_LIST",
     englishFriendlyName: "County Court at Central London Civil Daily Cause List",
-    welshFriendlyName: "Rhestr Achosion Dyddiol Sifil Llys Sirol Canolog Llundain",
+    welshFriendlyName: "Rhestr Achosion Dyddiol Sifil yn y Llys Sirol yng Nghanol Llundain",
     provenance: "MANUAL_UPLOAD",
     urlPath: "county-court-central-london-civil-daily-cause-list",
     isNonStrategic: true
@@ -113,7 +113,7 @@ export const mockListTypes: ListType[] = [
     id: 12,
     name: "COURT_OF_APPEAL_CRIMINAL_DAILY_CAUSE_LIST",
     englishFriendlyName: "Court of Appeal (Criminal Division) Daily Cause List",
-    welshFriendlyName: "Rhestr Achosion Dyddiol y Llys Apêl (Adran Droseddol)",
+    welshFriendlyName: "Rhestr Achosion Dyddiol y Llys Apêl (Adran Troseddol)",
     provenance: "MANUAL_UPLOAD",
     urlPath: "court-of-appeal-criminal-division-daily-cause-list",
     isNonStrategic: true
@@ -122,7 +122,7 @@ export const mockListTypes: ListType[] = [
     id: 13,
     name: "FAMILY_DIVISION_HIGH_COURT_DAILY_CAUSE_LIST",
     englishFriendlyName: "Family Division of the High Court Daily Cause List",
-    welshFriendlyName: "Rhestr Achosion Dyddiol Adran Deulu'r Uchel Lys",
+    welshFriendlyName: "Rhestr Achosion Dyddiol Adran Deulu yr Uchel Lys",
     provenance: "MANUAL_UPLOAD",
     urlPath: "family-division-high-court-daily-cause-list",
     isNonStrategic: true
@@ -149,7 +149,7 @@ export const mockListTypes: ListType[] = [
     id: 16,
     name: "MAYOR_CITY_CIVIL_DAILY_CAUSE_LIST",
     englishFriendlyName: "Mayor & City Civil Daily Cause List",
-    welshFriendlyName: "Rhestr Achosion Sifil Dyddiol y Maer a'r Ddinas",
+    welshFriendlyName: "Rhestr Achosion Dyddiol Llys Sifil y Maer a'r Ddinas",
     provenance: "MANUAL_UPLOAD",
     urlPath: "mayor-city-civil-daily-cause-list",
     isNonStrategic: true
@@ -158,7 +158,7 @@ export const mockListTypes: ListType[] = [
     id: 17,
     name: "SENIOR_COURTS_COSTS_OFFICE_DAILY_CAUSE_LIST",
     englishFriendlyName: "Senior Courts Costs Office Daily Cause List",
-    welshFriendlyName: "Rhestr Achosion Dyddiol Swyddfa Costau'r Uchel Lysoedd",
+    welshFriendlyName: "Rhestr Achosion Dyddiol Swyddfa Costau’r Uwchlysoedd",
     provenance: "MANUAL_UPLOAD",
     urlPath: "senior-courts-costs-office-daily-cause-list",
     isNonStrategic: true
@@ -188,7 +188,7 @@ export const mockListTypes: ListType[] = [
     id: 20,
     name: "BIRMINGHAM_ADMINISTRATIVE_COURT_DAILY_CAUSE_LIST",
     englishFriendlyName: "Birmingham Administrative Court Daily Cause List",
-    welshFriendlyName: "Rhestr Achosion Dyddiol y Llys Gweinyddol Birmingham",
+    welshFriendlyName: "Rhestr Achosion Dyddiol Llys Gweinyddol Birmingham",
     provenance: "MANUAL_UPLOAD",
     urlPath: "birmingham-administrative-court-daily-cause-list",
     isNonStrategic: true
@@ -197,7 +197,7 @@ export const mockListTypes: ListType[] = [
     id: 21,
     name: "LEEDS_ADMINISTRATIVE_COURT_DAILY_CAUSE_LIST",
     englishFriendlyName: "Leeds Administrative Court Daily Cause List",
-    welshFriendlyName: "Rhestr Achosion Dyddiol y Llys Gweinyddol Leeds",
+    welshFriendlyName: "Rhestr Achosion Dyddiol Llys Gweinyddol Leeds",
     provenance: "MANUAL_UPLOAD",
     urlPath: "leeds-administrative-court-daily-cause-list",
     isNonStrategic: true
@@ -206,7 +206,7 @@ export const mockListTypes: ListType[] = [
     id: 22,
     name: "BRISTOL_CARDIFF_ADMINISTRATIVE_COURT_DAILY_CAUSE_LIST",
     englishFriendlyName: "Bristol and Cardiff Administrative Court Daily Cause List",
-    welshFriendlyName: "Rhestr Achosion Dyddiol y Llys Gweinyddol Bryste a Chaerdydd",
+    welshFriendlyName: "Rhestr Achosion Dyddiol Llys Gweinyddol Bryste a Chaerdydd",
     provenance: "MANUAL_UPLOAD",
     urlPath: "bristol-cardiff-administrative-court-daily-cause-list",
     isNonStrategic: true
@@ -215,7 +215,7 @@ export const mockListTypes: ListType[] = [
     id: 23,
     name: "MANCHESTER_ADMINISTRATIVE_COURT_DAILY_CAUSE_LIST",
     englishFriendlyName: "Manchester Administrative Court Daily Cause List",
-    welshFriendlyName: "Rhestr Achosion Dyddiol y Llys Gweinyddol Manceinion",
+    welshFriendlyName: "Rhestr Achosion Dyddiol Llys Gweinyddol Manceinion",
     provenance: "MANUAL_UPLOAD",
     urlPath: "manchester-administrative-court-daily-cause-list",
     isNonStrategic: true
