11787310a06e52db7a59b5eff7b8416e066bb449
Daniel Alao <Daniel.Alao@justice.gov.uk>
2026-01-26 14:28:52 +0000

    merge to add list types table

--- apps/postgres/prisma/migrations/20251209141600_add_list_types/migration.sql
+++ apps/postgres/prisma/migrations/20251209141600_add_list_types/migration.sql
@@ -20,6 +20,7 @@ CREATE TABLE "list_types" (
     "is_non_strategic" BOOLEAN NOT NULL DEFAULT false,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
+    "deleted_at" TIMESTAMP(3),
 
     CONSTRAINT "list_types_pkey" PRIMARY KEY ("id")
 );
--- apps/postgres/prisma/migrations/20251223110301_add_deleted_at_to_list_types/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "list_types" ADD COLUMN     "deleted_at" TIMESTAMP(3);
