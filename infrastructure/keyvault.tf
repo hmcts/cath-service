006c6b29156e640e4899d1c7c5d56fe43c37625a
Linus Norton <linusnorton@gmail.com>
2026-03-16 15:19:28 +0000

    Fixes

--- infrastructure/keyvault.tf
+++ infrastructure/keyvault.tf
@@ -3,7 +3,8 @@ module "key_vault" {
 
   product                 = var.product
   env                     = var.env
-  object_id               = var.jenkins_AAD_objectId
+  object_id               = var.ci_service_principal_object_id
+  tenant_id               = var.tenant_id
   resource_group_name     = azurerm_resource_group.rg.name
   product_group_name      = var.product_group_name
   common_tags             = var.common_tags
