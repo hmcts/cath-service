110d2c57d0f4ef9047c0f8255c7e87676ed10207
Linus Norton <linusnorton@gmail.com>
2026-03-23 14:03:06 +0000

    fix: import pre-existing managed identity into terraform state
    The cath-stg-mi managed identity already exists in Azure but is not
    in the Terraform state. Add import block to adopt it.

--- infrastructure/keyvault.tf
+++ infrastructure/keyvault.tf
@@ -10,3 +10,9 @@ module "key_vault" {
   common_tags             = var.common_tags
   create_managed_identity = true
 }
+
+# Import pre-existing managed identity into Terraform state
+import {
+  to = module.key_vault.azurerm_user_assigned_identity.managed_identity[0]
+  id = "/subscriptions/74dacd4f-a248-45bb-a2f0-af700dc4cf68/resourceGroups/managed-identities-${var.env}-rg/providers/Microsoft.ManagedIdentity/userAssignedIdentities/${var.product}-${var.env}-mi"
+}
