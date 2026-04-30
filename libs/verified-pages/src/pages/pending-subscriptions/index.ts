a9a44b7edd95627919d07c51ce6fab475147e5df
KianKwa <kian.kwa@justice.gov.uk>
2026-04-30 10:47:34 +0100

    Merge branch 'feature/296-select-and-edit-list-type-subscriptions' into feature/510-subscribe-by-case-name-and-case-number
    # Conflicts:
    #	apps/web/.env.example
    #	libs/admin-pages/src/pages/manual-upload-summary/index.test.ts
    #	libs/notifications/src/notification/notification-service.test.ts
    #	libs/publication/src/processing/service.test.ts
    #	libs/publication/src/processing/service.ts
    #	libs/verified-pages/src/pages/subscription-confirmation-preview/index.ts

--- libs/verified-pages/src/pages/pending-subscriptions/index.ts
+++ libs/verified-pages/src/pages/pending-subscriptions/index.ts
@@ -1,6 +1,6 @@
 import { blockUserAccess, buildVerifiedUserNavigation, requireAuth } from "@hmcts/auth";
 import { getLocationById } from "@hmcts/location";
-import { Prisma } from "@hmcts/postgres";
+import { Prisma } from "@hmcts/postgres-prisma";
 import {
   createCaseSubscription,
   deletePendingCaseSubscriptions,
