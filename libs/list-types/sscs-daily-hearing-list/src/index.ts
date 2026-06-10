29039c211ef7c5dce799abeb150dde5b16d75d0d
junaidiqbalmoj <84805836+junaidiqbalmoj@users.noreply.github.com>
2026-06-10 17:07:28 +0100

    fix pdf generation

--- libs/list-types/sscs-daily-hearing-list/src/index.ts
+++ libs/list-types/sscs-daily-hearing-list/src/index.ts
@@ -3,5 +3,6 @@ import "./conversion/sscs-config.js"; // Register converters on module load
 // Business logic exports
 export * from "./email-summary/summary-builder.js";
 export * from "./models/types.js";
+export { importantInformationByListType } from "./pages/en.js";
 export * from "./pdf/pdf-generator.js";
 export * from "./rendering/renderer.js";
