5bee15d22bb2c9cd93d6db0c5ccefdeb3831a5c5
junaidiqbalmoj <84805836+junaidiqbalmoj@users.noreply.github.com>
2026-07-03 11:21:40 +0100

    Revert "fix: restore missing migration SQL files and remove orphan migration directories"
    This reverts commit f03d8d6cf71aa323b43251f0a38a6721e2202cad.

--- apps/postgres/prisma/migrations/20260522120000_readd_third_party_secret/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "third_party_secret" (
-    "name" VARCHAR(255) NOT NULL,
-    "value" VARCHAR(1000) NOT NULL,
-
-    CONSTRAINT "third_party_secret_pkey" PRIMARY KEY ("name")
-);
--- apps/postgres/prisma/migrations/20260701115926_third_party_subscription/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE "third_party_subscription" DROP CONSTRAINT "third_party_subscription_list_type_id_fkey";
-
--- AlterTable
-ALTER TABLE "third_party_subscription" ALTER COLUMN "id" DROP DEFAULT;
-
--- AlterTable
-ALTER TABLE "third_party_user" ALTER COLUMN "id" DROP DEFAULT;
