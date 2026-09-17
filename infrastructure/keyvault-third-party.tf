89d79b2f12d4bb499437b49fe6a7556e17f788a9
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2026-09-17 10:26:47 +0100

    revert to master to compare

--- infrastructure/keyvault-third-party.tf
+++ infrastructure/keyvault-third-party.tf
@@ -1,5 +1,5 @@
 module "third_party_key_vault" {
-  source = "git::https://github.com/hmcts/cnp-module-key-vault?ref=DTSPO-31965/remove-jenkins-ptl-access"
+  source = "git::https://github.com/hmcts/cnp-module-key-vault?ref=master"
 
   name                = "${var.product}-tp-${var.env}"
   product             = var.product
--- infrastructure/keyvault.tf
+++ infrastructure/keyvault.tf
@@ -1,5 +1,5 @@
 module "key_vault" {
-  source = "git::https://github.com/hmcts/cnp-module-key-vault?ref=DTSPO-31965/remove-jenkins-ptl-access"
+  source = "git::https://github.com/hmcts/cnp-module-key-vault?ref=master"
 
   product             = var.product
   env                 = var.env
